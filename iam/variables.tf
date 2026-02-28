5bad2796878bde58969d46cc7e05370c4f4fe8f4
maulana <ahmadmakyy14@gmail.com>
2026-02-28 13:15:20 +0700

    fill iam & add kms

---
+++ iam/variables.tf
@@ -0,0 +1,15 @@
+variable "KKE_IAM_EC2"{
+    type = string
+}
+
+variable "KKE_EC2_POLICY_NAME"{
+    type = string
+}
+
+variable "KKE_IAM_LAMBDA"{
+    type = string
+}
+
+variable "KKE_LAMBDA_POLICY_NAME"{
+    type = string
+}
