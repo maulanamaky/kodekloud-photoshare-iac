153af59a488f721d4b5c711247e0d66ebe39c541
maulana <ahmadmakyy14@gmail.com>
2026-02-28 17:22:12 +0700

    fill ec2 & edit alb also sm

---
+++ ec2/main.tf
@@ -0,0 +1,55 @@
+resource "aws_instance" "photoshare_web" {
+  ami           = var.ec2_config.ami
+  instance_type = var.ec2_config.type
+
+  subnet_id = [for k, v in aws_subnet.subnet : v.id if var.KKE_SUBNET[k].public == true][0]
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = var.ec2_config.name
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo dnf install -y docker git
+              sudo systemctl enable --now docker
+              sudo usermod -aG docker ec2-user
+
+              sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo chmod +x /usr/local/bin/docker-compose
+
+              cat <<EOT > docker-compose.yml
+              services:
+                api:
+                  image: kodekloud/photosharing-app
+                  container_name: photosharing-app
+                  ports:
+                  - "80:8000"
+                  env_file:
+                  - .env
+                  restart: always
+                  healthcheck:
+                    test: ["CMD", "curl", "-f", "http://localhost:8000/health"]
+                    interval: 30s
+                    timeout: 10s
+                    retries: 3
+                    start_period: 40s
+              EOT
+
+              cat <<-EOT > .env
+              S3_BUCKET=${aws_s3_bucket.photoshare_bucket.name}
+              AWS_SECRET_NAME=${aws_secretsmanager_secret.photoshare_sm.name}
+              EOT
+
+              sudo /usr/local/bin/docker-compose up -d
+  EOF
+
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2-role-profile"
+  role = aws_iam_role.ec2.name
+}
