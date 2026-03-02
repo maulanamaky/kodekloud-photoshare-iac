208e2d28ac673a309f828e0c33356c311d85d32d
maulana <ahmadmakyy14@gmail.com>
2026-03-02 17:23:06 +0700

    add variables cloudwatch

---
+++ cloudwatch/variables.tf
@@ -0,0 +1,9 @@
+variable "KKE_DASHBOARD_NAME"{
+    type = string
+    default = "photoshare-monitor"
+}
+
+variable "KKE_ALARM_LAMBDA_NAME"{
+    type = string
+    default = "PhotoShare-Lambda-Error-Alarm"
+}
